b2ec60fe2f4d215702c108ce4124da8db1863133
NF RIDOY <nfridoy@gmail.com>
2023-03-27 06:39:31 +0600

    Output added

--- Pi_Series.cpp
+++ Pi_Series.cpp
@@ -1,35 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n;
 
-isOdd(int odd)
-{
+bool check_prime(int n) {
+  bool is_prime = true;
 
-    if(odd/2 != 0)
-    {
-        return odd;
+  // 0 and 1 are not prime numbers
+  if (n == 0 || n == 1) {
+    is_prime = false;
+  }
+
+  for (int i = 2; i <= n / 2; ++i) {
+    if (n % i == 0) {
+      is_prime = false;
+      break;
     }
-    return 1;
+  }
+
+  return is_prime;
 }
 
 int main()
 {
-    int i,j,limit=100,four = 4;
-    double pi0=4,pi1=0,pi2=0;
+    long long int i,j,limit=pow(10,4),four = 4;
+    double pi0=4.0,pi1=0.0,pi2=0.0;
 
+    cout << "Limit(n) = " << limit << endl;
+
+    //for(i=1,j=3;i<=limit;i=i+4,j=j+4)
     for(i=1;i<=limit;i=i+4)
     {
         // Plus
-        pi1 = pi1 + (4/i);
+        pi1 = pi1 + (4.0/i);
+        //pi2 = pi2 - (4.0/j);
+        if(check_prime(i))
+            cout << "#" << i << " " << pi1 << "@";
     }
-    //cout >> pi1 >> endl;
-    printf("%lf \n", pi1);
+    cout << endl << endl << "Half of Series(+) = " << pi1 << endl << endl;
+    //printf("%lf \n", pi1);
     for(j=3;j<=limit;j=j+4)
     {
         // Minus
-        pi2 = pi2 - (4/j);
+        pi2 = pi2 - (4.0/j);
+        //if(j/check_prime(j)==0)
+        cout << pi2 << " ";
     }
-    //cout >> pi2 >> endl;
-    printf("%lf \n", pi2);
+    cout << endl << endl << "Half of Series(-) = " << pi2 << endl << endl;
+    cout << endl << "PI = " << pi1+pi2 << endl;
+    //printf("%lf \n", pi2);
 
     return 0;
 }
